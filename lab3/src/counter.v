eec0de8218bb3edd011fa13566f843ce8b79dc60
Class Account <eecs151-tad@c111-10.eecs.berkeley.edu>
2023-02-11 17:37:04 -0800

    update lab3

--- lab3/src/counter.v
+++ lab3/src/counter.v
@@ -6,21 +6,20 @@ module counter #(
     output [3:0] leds
 );
     wire [3:0] count;
-    wire [3:0] d;
-    assign leds = counter;
+    reg [3:0] d;
+    assign leds = count;
 
     always @(*) begin
         if (buttons[0])
-            data = counter + 4'd1;
+            d = count + 4'd1;
         else if (buttons[1])
-            data = counter - 4'd1;
+            d = count - 4'd1;
         else if (buttons[3])
-            data = 4'd0;
+            d = 4'd0;
         else
-            data = count;
-            
+            d = count;
     end
 
-    REGISTER #(4) counter (.q(count), .d(data), .clk(clk))
+    REGISTER #(4) counter (.q(count), .d(d), .clk(clk));
 endmodule
 
