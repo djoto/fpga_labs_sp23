14584432f2313a2e5239fd5cfd1a94a10b85ada6
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2023-02-11 18:44:33 -0800

    z1tops

---
+++ lab4/src/z1top.v
@@ -0,0 +1,41 @@
+`timescale 1ns/1ns
+`define CLOCK_FREQ 125_000_000
+
+`define REGISTER_FILE
+//`define ACCUMULATOR
+//`define CALCULATOR
+
+module z1top (
+    input CLK_125MHZ_FPGA,
+    input [3:0] BUTTONS,
+    input [1:0] SWITCHES,
+    output [5:0] LEDS
+);
+
+  // Button parser
+  // Sample the button signal every 500us
+  localparam integer B_SAMPLE_CNT_MAX = 0.0005 * `CLOCK_FREQ;
+  // The button is considered 'pressed' after 100ms of continuous pressing
+  localparam integer B_PULSE_CNT_MAX = 0.100 / 0.0005;
+
+  wire [3:0] buttons_pressed;
+  button_parser #(
+    .WIDTH(4),
+    .SAMPLE_CNT_MAX(B_SAMPLE_CNT_MAX),
+    .PULSE_CNT_MAX(B_PULSE_CNT_MAX)
+  ) bp (
+    .clk(CLK_125MHZ_FPGA),
+    .in(BUTTONS),
+    .out(buttons_pressed));
+
+`ifdef REGISTER_FILE
+   z1top_register_file z1top_rf(.CLK_125MHZ_FPGA(CLK_125MHZ_FPGA), .buttons_pressed(buttons_pressed), .SWITCHES(SWITCHES), .LEDS(LEDS));
+`endif
+`ifdef ACCUMULATOR
+   z1top_accumulator z1top_acc(.CLK_125MHZ_FPGA(CLK_125MHZ_FPGA), .buttons_pressed(buttons_pressed), .SWITCHES(SWITCHES), .LEDS(LEDS));
+`endif
+`ifdef CALCULATOR
+      z1top_calculator z1top_calc(.CLK_125MHZ_FPGA(CLK_125MHZ_FPGA), .buttons_pressed(buttons_pressed), .SWITCHES(SWITCHES), .LEDS(LEDS));
+`endif
+   
+endmodule
--- lab4/src/z1top_register_file.v
+++ lab4/src/z1top_register_file.v
@@ -1,28 +1,9 @@
-`timescale 1ns/1ns
-`define CLOCK_FREQ 125_000_000
-
-module z1top (
+module z1top_register_file (
   input CLK_125MHZ_FPGA,
-  input [3:0] BUTTONS,
+  input [3:0] buttons_pressed,
   input [1:0] SWITCHES,
   output [5:0] LEDS
 );
-
-  // Button parser
-  // Sample the button signal every 500us
-  localparam integer B_SAMPLE_CNT_MAX = 0.0005 * `CLOCK_FREQ;
-  // The button is considered 'pressed' after 100ms of continuous pressing
-  localparam integer B_PULSE_CNT_MAX = 0.100 / 0.0005;
-
-  wire [3:0] buttons_pressed;
-  button_parser #(
-    .WIDTH(4),
-    .SAMPLE_CNT_MAX(B_SAMPLE_CNT_MAX),
-    .PULSE_CNT_MAX(B_PULSE_CNT_MAX)
-  ) bp (
-    .clk(CLK_125MHZ_FPGA),
-    .in(BUTTONS),
-    .out(buttons_pressed));
 
   localparam AWIDTH = 5;
   localparam DWIDTH = 6;
