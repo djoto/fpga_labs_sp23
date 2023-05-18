3e03b0353d5b3ddddf5e2852e4813d00ca9c7de2
djoto <djordjegacic99tb@gmail.com>
2023-05-18 13:08:58 +0200

    lab1 finished

--- lab1/src/z1top.v
+++ lab1/src/z1top.v
@@ -4,6 +4,15 @@ module z1top(
   input [1:0] SWITCHES,
   output [5:0] LEDS
 );
+
   and(LEDS[0], BUTTONS[0], SWITCHES[0]);
   assign LEDS[5:1] = 0;
+
+//  wire a, b;
+//  and(a, BUTTONS[0], BUTTONS[1]);
+//  and(b, BUTTONS[2], BUTTONS[3]); 
+//  and(LEDS[1], a, b); 
+
+//  and(LEDS[1], BUTTONS[0], BUTTONS[1], BUTTONS[2], BUTTONS[3]);
+
 endmodule
